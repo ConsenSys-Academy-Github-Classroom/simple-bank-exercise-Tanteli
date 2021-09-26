40c4ea8c0348aa0e72b76967e54b560546186ee1
Andriamalala Tantely <andriatanteli@gmail.com>
2021-09-26 23:32:21 +0200

    Complete

--- contracts/SimpleBank.sol
+++ contracts/SimpleBank.sol
@@ -97,10 +97,14 @@ contract SimpleBank {
       // return the user's balance.
 
       // 1. Use a require expression to guard/ensure sender has enough funds
+        require(balances[msg.sender] >= withdrawAmount, "Not enough funds");
 
       // 2. Transfer Eth to the sender and decrement the withdrawal amount from
       //    sender's balance
+        balances[msg.sender] -= withdrawAmount;
+        msg.sender.transfer(withdrawAmount);
 
       // 3. Emit the appropriate event for this message
+      emit LogWithdrawal(msg.sender, withdrawAmount, balances[msg.sender]);
     }
 }
